b1fa3901279b330b9699eb29402cca32d2b8c84d
mrdavidlaing <david@davidlaing.com>
2011-09-06 18:19:34 +0100

    Remove ununsed code

--- src/CIAPI.AspNet.Controls/Core/ControlInjectorCssRegistrar.cs
+++ src/CIAPI.AspNet.Controls/Core/ControlInjectorCssRegistrar.cs
@@ -16,34 +16,12 @@ namespace CIAPI.AspNet.Controls.Core
 
         public void RegisterFromResource(WebControl control, Type resourceAssembly, string resourceNamePrefix, string resourceName)
         {
-//            var scriptManager = GetPageScriptManager(control);
-
-//            if (ResourceAddedAlready(scriptManager, resourceName)) return;
             var cssUrl = control.Page.ClientScript.GetWebResourceUrl(resourceAssembly,
                                                                      resourceNamePrefix + "." + resourceName);
 
             var cssControl = new Literal {Text = @"<link href=""" + cssUrl + @""" type=""text/css"" rel=""stylesheet"" />"};
             _injectionTarget.Controls.Add(cssControl);
-//            ScriptManager.RegisterClientScriptBlock(control.Page, control.GetType(), resourceName, css, false);
         }
 
-        private static ScriptManager GetPageScriptManager(Control control)
-        {
-            var scriptManager = ScriptManager.GetCurrent(control.Page);
-            if (scriptManager==null)
-            {
-                throw new ArgumentNullException("control", 
-                    string.Format("The {0} control must be on a page that has a ScriptManager on it", 
-                        control.GetType().FullName));
-            }
-            return scriptManager;
-        }
-
-        private static bool ResourceAddedAlready(ScriptManager scriptManager, string resourceName)
-        {
-            return scriptManager.Scripts.Where(s => s.Name.EndsWith(resourceName)).Count() > 0;
-        }
-
-        
     }
 }
